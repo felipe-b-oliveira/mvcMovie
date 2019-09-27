d1cedee416b1528c414a54e868be485413f4a427
Felipe-Oliveira <felipe.oliveira@efabrika.com.br>
2019-09-27 11:32:36 -0300

    Correcao na pasta exercicioLinq e alteracao de arquivos solution em primeiroProjeto e xadrez-console

---
+++ exercicioLinq/Entidades/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicioLinq.Entidades
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string nome, string email, double salario)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Salario = salario;
+        }
+    }
+}
---
+++ exercicioLinq/Program.cs
@@ -0,0 +1,59 @@
+﻿using exercicioLinq.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace exercicioLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho;
+
+            do
+            {
+                Console.WriteLine("Digite o caminho completo do arquivo: ");
+                caminho = Console.ReadLine();
+
+            } while (testarCaminho() == false);
+
+            List<Funcionario> listaFunc = new List<Funcionario>();
+
+            using (StreamReader sr = File.OpenText(caminho))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] campos = sr.ReadLine().Split(',');
+                    string nome = campos[0];
+                    string email = campos[1];
+                    double salario = double.Parse(campos[2], CultureInfo.InvariantCulture);
+                    listaFunc.Add(new Funcionario(nome, email, salario));
+                }
+            }
+
+            Console.WriteLine("Digite o salario: ");
+            double salarioFiltro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            var emails = listaFunc.Where(f => f.Salario > salarioFiltro).OrderBy(f => f.Email).Select(f => f.Email);
+            foreach (string email in emails)
+            {
+                Console.WriteLine(email);
+            }
+
+            bool testarCaminho()
+            {
+                if (File.Exists(caminho))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
