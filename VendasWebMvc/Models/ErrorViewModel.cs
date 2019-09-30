c913747547bbe679b00697444d5c6ab615b3a2ad
Felipe-Oliveira <felipe.oliveira@efabrika.com.br>
2019-09-30 10:32:25 -0300

    Commit inicial, criacao do projeto

---
+++ VendasWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VendasWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
